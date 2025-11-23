2989e859ff6d4a321ea18418493206c35d7bc711
codedex-JS <jakesechler@hotmail.com>
2025-11-22 20:48:01 -0500

    Added the races objects

--- DG_Character_build/DGraces.cpp
+++ DG_Character_build/DGraces.cpp
@@ -7,31 +7,31 @@ class Race : Player {
 protected:
     string race;
 public:
-    string Human = [1, 1, 1, 1, 1]
+    double Human = [1, 1, 1, 1, 1]
 
-    string Kanassan = [1, 0.9, 1.1, 1.05, 0.95]
+    double Kanassan = [1, 0.9, 1.1, 1.05, 0.95]
 
-    Kia = [0.95, 0.9, 1.1, 0.95, 1.1]
+    double Kia = [0.95, 0.9, 1.1, 0.95, 1.1]
 
-    Bio_Android = [1.15, 0.95, 1, 0.95, 0.95]
+    double Bio_Android = [1.15, 0.95, 1, 0.95, 0.95]
 
-    Bojack = [1.15, 0.85, 0.85, 1.2, 0.95]
+    double Bojack = [1.15, 0.85, 0.85, 1.2, 0.95]
 
-    Tuffle = [0.9, 0.9, 1.15, 1.15, 0.9]
+    double Tuffle = [0.9, 0.9, 1.15, 1.15, 0.9]
 
-    Yardratian = [1, 1.1, 0.9, 0.95, 1.05]
+    double Yardratian = [1, 1.1, 0.9, 0.95, 1.05]
 
-    Namekian = [1.05, 0.9, 1.1, 0.9, 1.05]
+    double Namekian = [1.05, 0.9, 1.1, 0.9, 1.05]
 
-    Majin = [1.1, 1, 0.9, 0.95, 1.05]
+    double Majin = [1.1, 1, 0.9, 0.95, 1.05]
 
-    Frost_Demon = [0.95, 1.1, 0.9, 0.9, 1.2]
+    double Frost_Demon = [0.95, 1.1, 0.9, 0.9, 1.2]
 
-    Demon = [1.05, 0.95, 0.9, 1.1, 1.1]
+    double Demon = [1.05, 0.95, 0.9, 1.1, 1.1]
 
-    Android = [0.9, 1.2, 1.2, 0.85, 0.85]
+    double Android = [0.9, 1.2, 1.2, 0.85, 0.85]
 
-    Half_Saiyan = [0.95, 0.95, 0.95, 1.1, 1.1]
+    double Half_Saiyan = [0.95, 0.95, 0.95, 1.1, 1.1]
 
-    Saiyan = [0.95, 0.9, 0.95, 1.1, 1.1]
+    double Saiyan = [0.95, 0.9, 0.95, 1.1, 1.1]
 };
