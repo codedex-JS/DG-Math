5fff4daed91e47c281505bbb081d5ac09124eb8d
codedex-JS <jakesechler@hotmail.com>
2025-11-22 20:33:29 -0500

    Added stuff

--- DG_Character_build/DGraces.cpp
+++ DG_Character_build/DGraces.cpp
@@ -5,6 +5,33 @@ using namespace std;
 
 class Race : Player {
 protected:
-    /* data */
+    string race;
 public:
-}
+    string Human = [1, 1, 1, 1, 1]
+
+    string Kanassan = [1, 0.9, 1.1, 1.05, 0.95]
+
+    Kia = [0.95, 0.9, 1.1, 0.95, 1.1]
+
+    Bio_Android = [1.15, 0.95, 1, 0.95, 0.95]
+
+    Bojack = [1.15, 0.85, 0.85, 1.2, 0.95]
+
+    Tuffle = [0.9, 0.9, 1.15, 1.15, 0.9]
+
+    Yardratian = [1, 1.1, 0.9, 0.95, 1.05]
+
+    Namekian = [1.05, 0.9, 1.1, 0.9, 1.05]
+
+    Majin = [1.1, 1, 0.9, 0.95, 1.05]
+
+    Frost_Demon = [0.95, 1.1, 0.9, 0.9, 1.2]
+
+    Demon = [1.05, 0.95, 0.9, 1.1, 1.1]
+
+    Android = [0.9, 1.2, 1.2, 0.85, 0.85]
+
+    Half_Saiyan = [0.95, 0.95, 0.95, 1.1, 1.1]
+
+    Saiyan = [0.95, 0.9, 0.95, 1.1, 1.1]
+};
